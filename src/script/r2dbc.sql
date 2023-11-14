235527965dd420a91b2a2de4a61e813b9709094a
juarezjuniorgithub <juarez.barbosa@oracle.com>
2023-11-14 14:31:27 +0000

    @juarezjuniorgithub

--- src/script/r2dbc.sql
+++ src/script/r2dbc.sql
@@ -3,5 +3,13 @@ CREATE TABLE customers (
 	name VARCHAR2(20 BYTE),
 	region VARCHAR2(20 BYTE)
 );
-
 COMMIT;
+
+INSERT INTO CUSTOMERS (id, name, region) VALUES (1, 'Juarez', 'Dublin');
+INSERT INTO CUSTOMERS (id, name, region) VALUES (2, 'David', 'Cork');
+INSERT INTO CUSTOMERS (id, name, region) VALUES (3, 'Joe', 'Limerick');
+INSERT INTO CUSTOMERS (id, name, region) VALUES (4, 'Karl', 'Belfast');
+INSERT INTO CUSTOMERS (id, name, region) VALUES (5, 'Patrick', 'Galway');
+COMMIT;
+
+SELECT * FROM CUSTOMERS;
